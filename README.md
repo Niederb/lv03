6d13df2400d77a43ac4473675a8585c28857ccd2
Thomas Niederberger <thomas@niederb.ch>
2020-09-11 13:41:43 +0200

    Initial commit of conversion functions

---
+++ README.md
@@ -0,0 +1,3 @@
+# ch1903
+ 
+Based on the formulas described in the document "Näherungsformeln für die Transformation zwischen Schweizer Projektionskoordinaten und WGS84" by swisstopo.
---
+++ src/lib.rs
@@ -0,0 +1,103 @@
+pub struct Wgs84 {
+    longitude: f64,
+    latitude: f64,
+    altitude: f64,
+}
+
+pub struct Ch1903 {
+    north: f64,
+    east: f64,
+    altitude: f64,
+}
+
+pub fn to_ch1903(wgs: Wgs84) -> Ch1903 {
+    let phi = (3600.0 * wgs.latitude - 169_028.66) / 10_000.0;
+    let phi_2 = phi.powi(2);
+    let phi_3 = phi.powi(3);
+    let lambda = (3600.0 * wgs.longitude - 26_782.5) / 10_000.0;
+    let lambda_2 = lambda.powi(2);
+    let lambda_3 = lambda.powi(3);
+
+    let e = 2_600_072.37 + 211_455.93 * lambda
+        - 10938.51 * lambda * phi
+        - 0.36 * lambda * phi_2
+        - 44.54 * lambda_3;
+    let n = 1_200_147.07 + 308_807.95 * phi + 3745.25 * lambda_2 + 76.63 * phi_2
+        - 194.56 * lambda_2 * phi
+        + 119.79 * phi_3;
+    let y = e - 2000_000.00;
+    let x = n - 1000_000.00;
+    let altitude = wgs.altitude - 49.55 + 2.73 * lambda + 6.94 * phi;
+    Ch1903 {
+        north: x,
+        east: y,
+        altitude,
+    }
+}
+
+pub fn to_wgs84(ch: Ch1903) -> Wgs84 {
+    let y = (ch.east - 600_000.0) / 1_000_000.0;
+    let y_2 = y.powi(2);
+    let y_3 = y.powi(3);
+    let x = (ch.north - 200_000.0) / 1_000_000.0;
+    let x_2 = x.powi(2);
+    let x_3 = x.powi(3);
+    let lambda = 2.6779094 + 4.728982 * y + 0.791484 * y * x + 0.1306 * y * x_2 - 0.0436 * y_3;
+    let phi = 16.9023892 + 3.238272 * x
+        - 0.270978 * y_2
+        - 0.002528 * x_2
+        - 0.0447 * y_2 * x
+        - 0.0140 * x_3;
+    let altitude = ch.altitude + 49.55 - 12.6 * y - 22.64 * x;
+
+    let lambda = lambda * 100.0 / 36.0;
+    let phi = phi * 100.0 / 36.0;
+    Wgs84 {
+        longitude: lambda,
+        latitude: phi,
+        altitude,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn toCh1903() {
+        let wgs = Wgs84 {
+            longitude: 7.44417,
+            latitude: 46.94658,
+            altitude: 542.8,
+        };
+        let ch = to_ch1903(wgs);
+        println!("east/north: {}/{}", ch.east, ch.north);
+        assert!((600_421.43 - ch.east).abs() < 2.0);
+        assert!((199_498.43 - ch.north).abs() < 2.0);
+    }
+
+    #[test]
+    fn toCh1903_2() {
+        let wgs = Wgs84 {
+            longitude: 8.730497076,
+            latitude: 46.044130339,
+            altitude: 542.8,
+        };
+        let ch = to_ch1903(wgs);
+        println!("east/north: {}/{}", ch.east, ch.north);
+        assert!((700_000.00 - ch.east).abs() < 2.0);
+        assert!((100_000.00 - ch.north).abs() < 2.0);
+    }
+
+    #[test]
+    fn toWgs84() {
+        let ch = Ch1903 {
+            east: 700_000.0,
+            north: 100_000.0,
+            altitude: 542.8,
+        };
+        let wgs = to_wgs84(ch);
+        //assert_eq!(8.730497076, wgs.longitude);
+        assert!((8.730497076 - wgs.longitude).abs() < 0.001);
+        assert!((46.044130339 - wgs.latitude).abs() < 0.001);
+    }
+}
